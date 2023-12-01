8c5a09c6a737328c101e6c4f7dd63f08452639f4
WewenGit <owenbons@gmail.com>
2023-12-01 14:26:12 +0100

    Bootstrap et éléments de base de la page d'accueil

--- projet_web/src/Controller/HomePageController.php
+++ projet_web/src/Controller/HomePageController.php
@@ -7,20 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
 {
-    #[Route('/home')]
+    #[Route('/home', name: 'home')]
     public function notifications(): Response
     {
-        // get the user information and notifications somehow
-        $userFirstName = '...';
-        $userNotifications = ['...', '...'];
 
         // the template path is the relative file path from `templates/`
-        return $this->render('base.html.twig', [
-            // this array defines the variables passed to the template,
-            // where the key is the variable name and the value is the variable value
-            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
-            'user_first_name' => $userFirstName,
-            'notifications' => $userNotifications,
-        ]);
+        return $this->render('base.html.twig', []);
     }
 }
